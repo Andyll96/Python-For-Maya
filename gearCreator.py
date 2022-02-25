234f9b951bf9e059cba029c806395a39ebcf71d1
Andy Llactahuamani - Surface <andy.llactahuamani@ryerson.ca>
2022-02-24 20:51:22 -0500

    extrudes and returns transform, constructor, extrude. Also added docstring

--- gearCreator.py
+++ gearCreator.py
@@ -1,6 +1,15 @@
 from maya import cmds
 
 def createGear(teeth=10, length=0.3):
+    """This function will create a gear with the given parameters
+
+    Args:
+        teeth (int, optional): The number of teeth to create. Defaults to 10.
+        length (float, optional): The length of the teeth. Defaults to 0.3.
+
+    Returns:
+        _type_: A tuple of the transform, constructor and extrude node
+    """
 
     # Teeth are every alternate face, so spans * 2
     spans = teeth * 2
@@ -16,5 +25,8 @@ def createGear(teeth=10, length=0.3):
     for face in sideFaces:
         # the add parameter will add the new face selection to the total selection, so that way we aren't selecting just one face
         cmds.select(f"{transform}.f[{face}]", add=True)
+
+    extrude = cmds.polyExtrudeFacet(localTranslateZ=length)[0]
+    return transform, constructor, extrude
 
 createGear()
