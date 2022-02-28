4e693731f1cf56af3c8a6f220f58c9bf3baf9096
Andy Llactahuamani - Surface <andy.llactahuamani@ryerson.ca>
2022-02-28 17:53:27 -0500

    added ControllerLibrary Class and added methods

--- conLibrary/controllerLibrary.py
+++ conLibrary/controllerLibrary.py
@@ -1,7 +1,7 @@
 from maya import cmds
 import os
 import json
-#import pprint
+import pprint
 
 USERAPPDIR = cmds.internalVar(userAppDir=True)
 # print(userAppDir)
@@ -14,5 +14,33 @@ def createDirectory(directory=DIRECTORY):
     Args:
         directory (str, optional): The directory to create. Defaults to DIRECTORY.
     """
-    if not os.path.exists(directory)
+    if not os.path.exists(directory):
         os.mkdir(directory)
+
+class ControllerLibrary(dict):
+    
+    def save(self, name, directory=DIRECTORY):
+        createDirectory()
+        path = os.path.join(directory, f"{name}.ma")
+        cmds.file(rename=path)
+        if cmds.ls(selection=True):
+            cmds.file(force=True, type="mayaAscii", exportSelected=True)
+        else:
+            cmds.file(save=True, type="mayaAscii", force=True)
+
+    def find(self, directory=DIRECTORY):
+        if not os.path.exists(directory):
+            return
+        
+        files = os.listdir(directory) 
+        mayaFiles = [f for f in files if f.endswith(".ma")]
+        print(mayaFiles)
+
+        for ma in mayaFiles:
+            name, ext = os.path.splittext(ma)
+            path = os.path.join(directory, ma)
+
+            # remember that this class is a dictionary 
+            self[name] = path
+
+        pprint.pprint(self)
--- conLibrary/main.py
+++ conLibrary/main.py
@@ -1,4 +1,9 @@
-from conLibrary import controllerLibrary
+from conLibrary import controllerLibrary as cl
 reload(controllerLibrary)
 
-controllerLibrary.createDirectory()
+lib = cl.ControllerLibrary()
+# lib.save("test")
+lib.find()
+# the lib object is a dictionary b/c it's class inherits from dictionary
+# the key is the file name, the value is the path
+print(lib)
