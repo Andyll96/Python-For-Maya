310d09fb96b857cdbef467a3e65275bbd5f9b718
allactah-Desktop <andy.llactahuamani@ryerson.ca>
2022-03-04 16:16:36 -0500

    created a custom widget

--- lightingManager.py
+++ lightingManager.py
@@ -30,10 +30,40 @@ class LightManager(QtWidgets.QDialog):
         createBtn.clicked.connect(self.createLight)
         layout.addWidget(createBtn, 0, 1)
 
+        scrollWidget = QtWidgets.QWidget()
+        scrollWidget.setSizePolicy(QtWidgets.QSizePolicy.Maximum)
+        self.scrollLayout = QtWidgets.QVBoxLayout(scrollWidget)
+
+        scrollArea = QtWidgets.QScrollArea()
+        scrollArea.setWidgetResizable(True)
+        scrollArea.setWidget(scrollWidget)
+        layout.addWidget(scrollArea, 1, 0, 1, 2)
+
     def createLight(self):
         lightType = self.lightTypeCB.currentText()
         func = self.lightTypes[lightType]
         light = func()
+        widget = LightWdiget(light)
+        self.scrollLayout.addWidget(widget)
+
+class LightWdiget(QtWidgets.QWidget):
+
+    def __init__(self, light):
+        super(LightWdiget, self).__init__()
+        light = pm.PyNode(light)
+
+        self.light = light
+        self.buildUI()
+
+    def buildUI(self):
+        layout = QtWidgets.QGridLayout(self)
+        self.name = QtWidgets.QCheckBox(str(self.light.getTransform()))
+        self.name.setChecked(self.light.visibility.get())
+        # lambda is the same as writing
+        # def setLightVisibility(val):
+        #   self.light.visibility.set(val)
+        self.name.toggled.connect(lambda val: self.light.getTransform().visibility.set(val))
+        layout.addWidget(self.name, 0, 0)
 
 def showUI():
     ui = LightManager()
