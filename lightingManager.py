278839e8b4a7b02051d5da75eba13d06561e999e
allactah-Desktop <andy.llactahuamani@ryerson.ca>
2022-03-04 21:17:40 -0500

    added control to change color of light, populate existing lights, and refresh button

--- lightingManager.py
+++ lightingManager.py
@@ -17,6 +17,17 @@ class LightManager(QtWidgets.QDialog):
         super(LightManager, self).__init__()
         self.setWindowTitle('Lighting Manager')
         self.buildUI()
+        self.populate()
+
+    def populate(self):
+        while self.scrollLayout.count():
+            widget = self.scrollLayout.takeAt(0).widget()
+            if widget:
+                widget.setVisible(False)
+                widget.deleteLater()
+
+            for light in pm.ls(type=["areaLight", "spotLight", "pointLight", "directionalLight","volumeLight"]):
+                self.addLight(light)
 
     def buildUI(self):
         layout = QtWidgets.QGridLayout(self)
@@ -32,13 +43,17 @@ class LightManager(QtWidgets.QDialog):
         layout.addWidget(createBtn, 0, 1)
 
         scrollWidget = QtWidgets.QWidget()
-        scrollWidget.setSizePolicy(QtWidgets.QSizePolicy.Maximum)
+        scrollWidget.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
         self.scrollLayout = QtWidgets.QVBoxLayout(scrollWidget)
 
         scrollArea = QtWidgets.QScrollArea()
         scrollArea.setWidgetResizable(True)
         scrollArea.setWidget(scrollWidget)
         layout.addWidget(scrollArea, 1, 0, 1, 2)
+
+        refreshBtn = QtWidgets.QPushButton('Refresh')
+        refreshBtn.clicked.connect(self.populate)
+        layout.addWidget(refreshBtn, 2, 1)
 
     def createLight(self):
         lightType = self.lightTypeCB.currentText()
@@ -52,9 +67,9 @@ class LightManager(QtWidgets.QDialog):
         widget.onSolo.connect(self.onSolo)
 
     def onSolo(self, value):
-        LightWidget = self.findChildren(LightWidget)
+        lightWidgets = self.findChildren(LightWidget)
         # print(LightWidget)
-        for widget in LightWidget:
+        for widget in lightWidgets:
             if widget != self.sender():
                 widget.disableLight(value)
 
@@ -87,8 +102,8 @@ class LightWidget(QtWidgets.QWidget):
         soloBtn.toggled.connect(lambda val: self.onSolo.emit(val))
         layout.addWidget(soloBtn, 0, 1)
 
-        deleteBtn = QtWidgets.QPushButton("X")
-        deleteBtn.clicked().connect(self.deleteLight)
+        deleteBtn = QtWidgets.QPushButton(" X ")
+        deleteBtn.clicked.connect(self.deleteLight)
         deleteBtn.setMaximumWidth(10)
         layout.addWidget(deleteBtn, 0, 2)
 
@@ -98,6 +113,30 @@ class LightWidget(QtWidgets.QWidget):
         intensity.setValue(self.light.intensity.get())
         intensity.valueChanged.connect(lambda val: self.light.intensity.set(val))
         layout.addWidget(intensity, 1, 0, 1, 2)
+
+        self.colorBtn = QtWidgets.QPushButton()
+        self.colorBtn.setMaximumWidth(20)
+        self.colorBtn.setMaximumHeight(20)
+        self.setButtonColor()
+        self.colorBtn.clicked.connect(self.setColor)
+        layout.addWidget(self.colorBtn, 1, 2)
+
+    def setButtonColor(self, color=None):
+        if not color:
+            color = self.light.color.get()
+
+        assert len(color) == 3, "You must provide a list of 3 colors"
+
+        r,g,b = [c*255 for c in color]
+        self.colorBtn.setStyleSheet(f'background-color: rgba({r},{g},{b}, 1.0)')
+
+    def setColor(self):
+        lightColor = self.light.color.get()
+        color = pm.colorEditor(rgbValue=lightColor)
+        r,g,b,a = [float(c) for c in color.split()]
+        color = (r,g,b)
+        self.light.color.set(color)
+        self.setButtonColor(color)
         
     def disableLight(self, value):
         self.name.setChecked(not value)
@@ -107,7 +146,7 @@ class LightWidget(QtWidgets.QWidget):
         self.setVisible(False)
         self.deleteLater()
 
-        pm.delete(self.light.getTransform(j))
+        pm.delete(self.light.getTransform())
 
 def showUI():
     ui = LightManager()
